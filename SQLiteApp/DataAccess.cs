e0f14874d14157c3eea823bff68677f620e6e99d
AOFones <guinwwo456@gmail.com>
2024-11-27 22:32:21 +0700

    Initial commit

---
+++ SQLiteApp/DataAccess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace SQLiteApp
+{
+    internal class DataAccess
+    {
+        public async static void InitializeDatabase()
+        {
+            using (SqliteConnection db =
+            new SqliteConnection($"Filename=sqliteSample.db"))
+            {
+                db.Open();
+                String tableCommand = "CREATE TABLE IF NOT " +
+                "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
+                "Text_Entry NVARCHAR(2048) NULL)";
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+                createTable.ExecuteReader();
+            }
+        }
+    }
+}
